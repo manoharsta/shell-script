777bfd288b38d83ee8f64bb5a6f73d83955457d4
manoharsta <manoharsta11@gmail.com>
2023-08-25 12:32:59 +0530

    assignment

--- 10-assignment.sh
+++ 10-assignment.sh
@@ -3,15 +3,18 @@
 DATE=$(date)
 SCRIPT_NAME=$0
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
-
-for i in $@
-do
-    yum install $i -y &>>$LOGFILE
+VALIDATE(){
     if [$? -ne 0]
     then
         echo -e "\e[31m Installation Failure"
     else
         echo -e "\e[32m Installation Success"
     fi
+}
+
+for i in $@
+do
+    yum install $i -y &>>$LOGFILE
+    VALIDATE
 done
 
