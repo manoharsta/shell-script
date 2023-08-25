c579bc89b182d408a7a4867047a7aeaaef95a158
manoharsta <manoharsta11@gmail.com>
2023-08-25 13:36:19 +0530

    Assignment finished

--- 10-assignment.sh
+++ 10-assignment.sh
@@ -20,3 +20,43 @@ VALIDATE $?
 # done
 
 # VALIDATE 
+#!/bin/bash
+​
+# DATE=$(date +%F)
+# SCRIPTNAME=$0
+# LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# Y="\e[33m"
+# N="\e[0m"
+# ​
+# if [ $USERID -ne 0 ]
+# then
+#     echo "Run the script with root access"
+#     exit 1
+# fi
+# ​
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$i Installation is $R Failure$N"
+#     else
+#         echo -e "$i Installation is $G Success$N"
+#     fi
+# }
+# ​
+# PACKAGE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         yum install $i -y >>$LOGFILE
+#         VALIDATE $? $i
+#     else
+#         echo -e "$i $Y already installed$N"
+#     fi
+# }
+# for i in $@
+# do
+#     yum list installed | grep $i >>$LOGFILE
+#     PACKAGE $?
+# done
