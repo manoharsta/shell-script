ce2c54659534e173ff300e024ff5c19ee325d368
manoharsta <manoharsta11@gmail.com>
2023-08-25 11:07:52 +0530

    special variables

--- 07-special-variable.sh
+++ 07-special-variable.sh
@@ -3,10 +3,7 @@
 VAR1=$1
 VAR2=$2
 
-echo "$VAR1"
-echo "$VAR2"
-
-echo " NUmber of variables: $#"
+echo "NUmber of variables: $#"
 echo "Exit status code is : $?"
 echo "Variables are: $@"
 echo "SCript name : $0"
